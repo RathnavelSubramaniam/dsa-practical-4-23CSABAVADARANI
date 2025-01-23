18cc938e9a506cd3bced7e08e932f489b2f38d6b
23CSABAVADARANI <139536337+23CSABAVADARANI@users.noreply.github.com>
2025-01-23 05:11:00 +0000

    submit4

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -20,20 +20,17 @@ public class Array_Insertion {
         e=sc.nextInt();
         arrayInsert(arr,p,e,len);
     }
-    static void arrayInsert(int[]arr,int p, int e,int len)
-    {
-    for(int i=len-1;i>(p-1);i--)
-    {
-        arr[i+1]=arr[i];
-    }
-    arr[p-1]=e;
-System.out.println("After Inserting");
-System.out.println("Length of the array:+" +len);
-for(int i=0;i<=len;i++)
-{
-System.out.println("arr["+i+"]="+arr[i]);
+    
+static void arrayInsert(int[] arr, int p, int e, int len) {
+        for (int i = len - 1; i >= (p - 1); i--) {
+            arr[i + 1] = arr[i]; 
+        }
+        arr[p - 1] = e;
+        
+        System.out.println("After Inserting");
+        System.out.println("Length of the array:"+(len+1));
+        for (int i = 0; i <=len; i++) {
+            System.out.println("arr["+i+"]="+arr[i]);
+        }
     }
 }
-}
-
-
